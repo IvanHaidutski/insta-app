e2bf8019f5a1a550fd09c66771738a2c7f13bcb4
Ivan <haidutski@gmail.com>
2024-12-22 21:31:43 +0300

    daily commit

---
+++ src/app/(website)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <div>Page</div>;
+};
+//Server Action Onboard user
+//200 || 201
+export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +20,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body suppressHydrationWarning className={inter.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
