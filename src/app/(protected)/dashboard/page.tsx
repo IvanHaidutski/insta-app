3d439dbc3d4b4f3f3c5c1a0682e094dd59282e46
Ivan <haidutski@gmail.com>
2024-12-31 01:40:10 +0300

    WIP

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const Page = async (props: Props) => {
+  //WIP: Server Action Onboard the user
+  //WIP: 200 || 201
+
+  return <div>Page</div>;
+};
+
+export default Page;
