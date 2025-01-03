9bcab13f4fc1781390d74b5b098746f085c9fbee
Ivan <haidutski@gmail.com>
2025-01-03 23:55:35 +0300

    dayly commit

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -1,4 +1,7 @@
-
+import { RocketDuoToneWhite, SettingsDuoToneWhite } from "@/icons";
+import { AutomationDuoToneWhite } from "@/icons/automation-duotone-white";
+import { HomeDuoToneWhite } from "@/icons/home-duo-tone-white";
+import { v4 as uuid } from "uuid";
 
 type FieldProps = {
   label: string;
@@ -9,4 +12,25 @@ type SideBarProps = {
   icon: React.ReactNode;
 } & FieldProps;
 
-export const SIDEBAR_MENU: SideBarProps[] = [];
+export const SIDEBAR_MENU: SideBarProps[] = [
+  {
+    id: uuid(),
+    label: "home",
+    icon: <HomeDuoToneWhite />,
+  },
+  {
+    id: uuid(),
+    label: "automation",
+    icon: <AutomationDuoToneWhite />,
+  },
+  {
+    id: uuid(),
+    label: "integrations",
+    icon: <RocketDuoToneWhite />,
+  },
+  {
+    id: uuid(),
+    label: "settings",
+    icon: <SettingsDuoToneWhite />,
+  },
+];
